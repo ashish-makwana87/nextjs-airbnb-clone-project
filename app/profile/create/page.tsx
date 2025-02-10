53ebe7780f2a66cd7e0c7d1777855ded402b1511
Ashish Makwana <ashishn.makwana@gmail.com>
2025-02-10 18:32:03 +0530

    form components setup

---
+++ app/profile/create/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProfileCreatePage() {
+  return (
+    <div>ProfileCreatePage</div>
+  )
+}
+
+export default ProfileCreatePage
---
+++ components/form/Buttons.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useFormStatus } from "react-dom";
+import { Button } from "../ui/button";
+import { TbReload } from "react-icons/tb";
+
+type btnSize = "sm" | "default" | "lg";
+
+type submitButtonProps = {
+  text?: string;
+  size?: btnSize;
+  className?: string;
+};
+
+export const SubmitButton = ({
+  size = "lg",
+  text = "submit",
+  className = "",
+}: submitButtonProps) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      type='submit'
+      disabled={pending}
+      size={size}
+      className={`capitalize ${className}`}
+    >
+      {pending ? (
+        <>
+          <TbReload className='mr-2 w-4 h-4 animate-spin' />
+          Please wait
+        </>
+      ) : (
+        text
+      )}
+    </Button>
+  );
+};
+
